00537b16768d161ffdc43a11b93bcc1e8353e7b9
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2021-10-20 12:27:33 -0400

    logon basics (NOT FINISHED)

--- logon.py
+++ logon.py
@@ -1,2 +1,60 @@
-import
-from tkinter import Tk()
+#IMPORTS
+import sqlite3
+from tkinter import *
+import math
+import random
+import smtplib
+from tkinter import messagebox
+
+# Database and Table
+con = sqlite3.connect('userdata.db')
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS record(
+    name text,
+    username text,
+    pasword text,
+    email text
+)
+''')
+con.commit()
+
+# Window
+root = Tk()
+root.geometry('500x450')
+root.title('Verification')
+root.resizable(False, False)
+
+# Entry Widget Data Var
+
+fullname = StringVar()
+username = StringVar()
+password = StringVar()
+username_lo = StringVar()
+password_lo = StringVar()
+email = StringVar()
+otp = StringVar()
+
+# Insert Data on Record
+def insert_record():
+    count = 0
+    warn = ''
+    if fullname.get() == '':
+        warn = "NAME FIELD CANNOT BE LEFT EMPTY!"
+    else:
+        count += 1
+    
+    if username.get() == '':
+        warn = "USERNAME FIELD CANNOT BE LEFT EMPTY!"
+    else:
+        count += 1
+    
+    if email.get() == '':
+        warn = "EMAIL FIELD CANNOT BE LEFT EMPTY!"
+    else:
+        count += 1
+    
+    if password.get() == '':
+        warn = "AAAAAA PASSWORD CANNOT BE LEFT EMPTY AAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    else:
+        count += 1
+        
