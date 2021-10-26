0ded7346a2cc635d9d215dfa6ba8ad729149b24e
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2021-10-26 13:29:15 -0400

    elaborate on logon (NOT FINISHED)

--- logon.py
+++ logon.py
@@ -57,4 +57,47 @@ def insert_record():
         warn = "AAAAAA PASSWORD CANNOT BE LEFT EMPTY AAAAAAAAAAAAAAAAAAAAAAAAAAA"
     else:
         count += 1
+    if count == 4:
+        con = sqlite3.connect('userdata.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO record VALUES(:name, :username, :password, :email)", {
+            'name': fullname.get(),
+            'username': username.get(),
+            'password': password.get(),
+            'email': email.get()
+        })
+        con.commit()
+
+# Select Date From Records
+def login_response():
+    try:
+        con = sqlite3.connect('userdata.db')
+        c = con.cursor()
+        for row in c.execute("Select * from record"):
+            usern = row[1]
+            passwo = row[2]
+
+    except Exception as ep:
+        messagebox.showerror('', ep)
+    count = 0
+    if username_lo.get() == '':
+        warn = "USERNAME FIELD CANNOT BE LEFT EMPTY!"
+    else:
+        count += 1
+
+    if password_lo.get() == '':
+        warn = "PASSWORD FIELD CANNOT BE LEFT EMPTY!"
+    else:
+        count += 1
+    if count == 2:
+        if username_lo.get() == usern and password_lo.get() == passwo:
+            messagebox.showinfo('', 'Login Successful')
+            root.destroy()
+            import main
+        else:
+            messagebox.showerror('', 'Username or Password is incorrect')
+    else:
+        messagebox.showerror('', 'Username or Password is incorrect')
+        
+
         
