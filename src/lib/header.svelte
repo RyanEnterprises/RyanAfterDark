77f71b078a708ece7d0f083bbecd57c52c33b73e
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-05-01 16:48:24 -0400

    feat: :sparkles: made the header

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,9 +1,26 @@
 <script>
     import '../app.css';
+    const logo = {
+        standard: '../../img/standard.png',
+        dark: '../../img/black.png',
+        light: '../../img/white.png',
+        christmas: '../../img/christmas.png'
+    };
+    // IF the user prefers dark mode, then the logo should be light
+    // However, if the date is in the month of december, then the logo should be christmas
+    // All other cases, the logo should be standard
+
+    let logoUrl = logo.standard;
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        logoUrl = logo.light;
+    } else if (new Date().getMonth() === 11) {
+        logoUrl = logo.christmas;
+    }
 </script>
 
 <header>
-    <div class="flex justify-between p-6 dark:bg-black">
-        <img src="../../img/standard.png" alt="logo" />
+    <div class="flex justify-between p-6 dark:bg-black text-primary dark:text-white">
+        <img src={logoUrl} alt="logo" />
+        <a href="#extras" class="hover:text-blue-500">Extras</a>
     </div>
 </header>
