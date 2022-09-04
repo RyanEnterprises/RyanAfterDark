3f377558c887a119ecfa82867ed1fc6c7a0e3b9f
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-09-03 22:26:34 -0400

    chore: :fire: BURN BABY BURN

--- src/components/iconMachine.svelte
+++
@@ -1,6 +0,0 @@
-
-<script>
-    import '../app.css';
-    const bootstrapIcons = '../../node_modules/bootstrap/icons/';
-    
-</script>
--- src/views/404/404.js
+++
@@ -1,5 +0,0 @@
-import App from './404.svelte'
-
-const app = new App({target: document.getElementById('app')})
-
-export default app
