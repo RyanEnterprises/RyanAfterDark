8d343ef2d9ea00ebc1164cef086db6400491e6f5
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-09-10 21:19:10 -0400

    update base in astroConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(), sitemap(), svelte(), react()],
-  site: 'https://ryanenterprises.github.io/RyanAfterDark',
+  site: 'https://ryanenterprises.github.io',
+  base: "/RyanAfterDark"
 });
